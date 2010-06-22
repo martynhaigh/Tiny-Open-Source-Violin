3b6d8a7298599f28b5390ade53daa33b8bb7461e
Martyn Haigh <martyn.haigh@gmail.com>
2010-06-22 11:25:43 -0400

    version 1.2

--- Trunk/src/com/martynhaigh/violin/launchViolin.java
+++ Trunk/src/com/martynhaigh/violin/launchViolin.java
@@ -2,21 +2,62 @@ package com.martynhaigh.violin;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class launchViolin extends Activity {
+public class LaunchViolin extends Activity  {
 	
     private static final int MENU_ABOUT = Menu.FIRST;
     private static final int MENU_QUIT = MENU_ABOUT + 1;
-	
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+      
+        SharedPreferences appPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        int acceptedCodeVersion = appPreferences.getInt("acceptedVersion", 0);
+        Log.i("start","Accepted Version" + acceptedCodeVersion);
+        if(acceptedCodeVersion == 0 || acceptedCodeVersion != getAppVersionCode(getBaseContext())) {
+
+    		String versionNumber = "";
+    		
+    		try {
+    			versionNumber = getBaseContext().getPackageManager().getPackageInfo(this.getPackageName(), 0).versionName;
+    		} catch (NameNotFoundException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}
+    		String whatsNew = "";
+    		String headerText = getResources().getString(R.string.whatsNewText) + " " + versionNumber;
+    		String[] arrayContents = getResources()
+    				.getStringArray(R.array.whatsNew);
+    		for (String s : arrayContents) {
+    			whatsNew += "* " + s + "\n";
+    		}       	
+        	 new AlertDialog.Builder(this)
+             .setIcon(android.R.drawable.ic_dialog_alert)
+             .setTitle(headerText)
+             .setMessage(whatsNew)
+             .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                 public void onClick(DialogInterface dialog, int which) {
+                	 dialog.dismiss();
+                 }
+             })
+             .show();        	
+			SharedPreferences.Editor editor = appPreferences.edit();
+	        editor.putInt("acceptedVersion",  getAppVersionCode(getBaseContext()));
+	        editor.commit(); // Very important
+        }
+     
     }
     
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -27,24 +68,36 @@ public class launchViolin extends Activity {
 	
 	public boolean onOptionsItemSelected(MenuItem item) {
 	    switch (item.getItemId()) {
-	    case MENU_ABOUT:
-
-			new AlertDialog.Builder(this).setTitle(R.string.app_name).setMessage(
-					R.string.about_text).setPositiveButton("OK",
-					new AlertDialog.OnClickListener() {
-						public void onClick(DialogInterface dialog, int which) {
-							dialog.dismiss();
-						}
-					}).setCancelable(false).create().show();
-	    	
-	    	
-	    	return true;
-	    case MENU_QUIT:
-	    	quitApplication();
-	        return true;
+		    case MENU_ABOUT:
+	
+				new AlertDialog.Builder(this).setTitle(R.string.app_name).setMessage(
+						R.string.about_text).setPositiveButton("OK",
+						new AlertDialog.OnClickListener() {
+							public void onClick(DialogInterface dialog, int which) {
+								dialog.dismiss();
+							}
+						}).setCancelable(false).create().show();
+		    	
+		    	
+		    	return true;
+		    case MENU_QUIT:
+		    	quitApplication();
+		        return true;
 	    }
 	    return false;
 	}		
+
+	public int getAppVersionCode(Context context) {
+	    if ( context!= null )
+	    {
+	        try {
+	            return context.getPackageManager().getPackageInfo(this.getPackageName(), 0).versionCode;
+	        } catch (NameNotFoundException e) {
+	            // App not installed!
+	        }
+	    }
+	    return -1;
+	}
 
 	public void quitApplication() {
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -54,7 +107,7 @@ public class launchViolin extends Activity {
 						new DialogInterface.OnClickListener() {
 							public void onClick(DialogInterface dialog,
 									int id) {
-								launchViolin.this.finish();
+								LaunchViolin.this.finish();
 							}
 						}).setNegativeButton("No",
 						new DialogInterface.OnClickListener() {
--- Trunk/src/com/martynhaigh/violin/violinView.java
+++ Trunk/src/com/martynhaigh/violin/violinView.java
@@ -1,9 +1,12 @@
 package com.martynhaigh.violin;
 
+import java.util.Random;
+
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
@@ -12,20 +15,33 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
-public class violinView extends View {
+public class ViolinView extends View {
 
-	public violinView(Context context, AttributeSet attrs) {
+	public ViolinView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		// TODO Auto-generated constructor stub
+		violin = BitmapFactory
+				.decodeResource(getResources(), R.drawable.violin);
+		bow = BitmapFactory.decodeResource(getResources(), R.drawable.bow);
+		
+		scaledViolin = Bitmap.createBitmap(violin);
+		highlightColour = new Paint();
+		highlightColour.setColor(getResources().getColor(R.color.highlight));
+		violinColour = new Paint();
+		violinColour.setColor(getResources().getColor(R.color.background));
+		subtleColour = new Paint();
+		subtleColour.setColor(getResources().getColor(R.color.subtle));
 		
 	}
 
-	Bitmap violin = BitmapFactory.decodeResource(getResources(),
-			R.drawable.violin);
-	Bitmap bow = BitmapFactory.decodeResource(getResources(), R.drawable.bow);
+	Bitmap violin, bow, scaledViolin;
 	Float lastX, lastY;
-	Boolean bowDown = false;
+	Boolean bowDown = false, resize = false;
 	MediaPlayer mp;
+	double xOff, counter = 0;
+	boolean top = false, left = false, calculateCentre = true;
+	float vX1 = -100, vX2, vY1, vY2, bitmapRatio = 0;
+	Paint highlightColour, violinColour, subtleColour;
+	int touchArea = 20;
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
@@ -33,21 +49,84 @@ public class violinView extends View {
 		lastX = event.getX();
 		lastY = event.getY();
 		if (action == MotionEvent.ACTION_UP) {
-			bowDown = false;
-			mp.release();
-			Log.i("violin", "finger up on violin");
+			if(resize)
+				resize = false;
+			else {
+				bowDown = false;
+				mp.release();
+				Log.i("violin", "finger up on violin");
+			}
 		} else if (action == MotionEvent.ACTION_MOVE) {
-			Log.i("violin", "finger move on violin");
-		} else if (action == MotionEvent.ACTION_DOWN) {
-			bowDown = true;
-			mp = MediaPlayer.create(getContext(), R.raw.music);
-			mp.setOnCompletionListener(new OnCompletionListener() {
-				@Override
-				public void onCompletion(MediaPlayer mp) {
-					mp.release();
+			
+			if(resize) {
+
+
+					int width = violin.getWidth();
+			        int height = violin.getHeight();
+			        
+			        int newHeight;
+			        if(top) {
+			        	newHeight =  (int) (getMeasuredHeight() - 2*lastY);
+			        } else {
+			        	newHeight =  (int) (getMeasuredHeight() - 2*(getMeasuredHeight() - lastY));
+			        }
+			       
+			        if(newHeight < 10) newHeight = 10;
+			        if(newHeight > getMeasuredHeight()-20) newHeight = getMeasuredHeight()-20;
+			        
+			        int newWidth = (int) (newHeight/bitmapRatio);
+			        
+			        Log.i("viold resize", "width = " + newWidth + " - ratio = " + bitmapRatio);
+			        
+					float scaleWidth = ((float) newWidth) / width;
+					float scaleHeight = ((float) newHeight) / height;
+					Matrix matrix = new Matrix();
+					matrix.postScale(scaleWidth, scaleHeight);
+					scaledViolin = Bitmap.createBitmap(violin, 0, 0,
+	                          violin.getWidth(), violin.getHeight(), matrix, true);
+					calculateCentre = true;
+					
+
+			} else {
+				double xNoise = lastX * 0.01;
+				double yNoise = lastY * 0.01;
+				double counterNoise = counter++ * 0.02;
+	
+				xOff = Noise.createNoise(xNoise, yNoise, counterNoise);
 				}
-			});			
-			mp.start();
+		} else if (action == MotionEvent.ACTION_DOWN) {
+			
+			if (((lastX > vX1 - touchArea && lastX < vX1 + touchArea) || (lastX > vX2 - touchArea && lastX < vX2 + touchArea))
+					&& ((lastY > vY1 - touchArea && lastY < vY1 + touchArea) || (lastY > vY2 - touchArea && lastY < vY2 + touchArea))) {
+				resize = true;
+				if (lastX > vX1 - touchArea && lastX < vX1 + touchArea) {
+					left = true;
+				} else {
+					left = false;
+				}
+				if(lastY > vY1 - touchArea && lastY < vY1 + touchArea) {
+					top = true;
+				} else {
+					top = false;
+				}
+			} else {
+				bowDown = true;
+				Random random = new Random();
+				int randNum = random.nextInt(31);
+				if (randNum > 20) {
+					mp = MediaPlayer.create(getContext(), R.raw.violinmusic1);
+				} else if (randNum < 10) {
+					mp = MediaPlayer.create(getContext(), R.raw.violinmusic2);
+				} else {
+					mp = MediaPlayer.create(getContext(), R.raw.violinmusic3);
+				}
+				mp.setOnCompletionListener(new OnCompletionListener() {
+					public void onCompletion(MediaPlayer mp) {
+						mp.release();
+					}
+				});
+				mp.start();
+			}
 
 		}
 		this.invalidate();
@@ -56,14 +135,65 @@ public class violinView extends View {
 
 	@Override
 	protected void onDraw(Canvas canvas) {
-		Paint violinColour = new Paint();
-		violinColour.setColor(getResources().getColor(R.color.background));
-		canvas.drawBitmap(violin, (getMeasuredWidth() - violin.getWidth()) / 2,
-				(getMeasuredHeight() - violin.getHeight()) / 2, violinColour);
-		if (bowDown)
-			canvas.drawBitmap(bow, lastX - (bow.getWidth() / 2), lastY
-					- (bow.getHeight() / 2), violinColour);
+		if(bitmapRatio == 0) {	
+			bitmapRatio = (float)scaledViolin.getHeight() / (float)scaledViolin.getWidth();
+			Log.i("bitmapRatio",  scaledViolin.getHeight() +"/"+ scaledViolin.getWidth() + "="+ bitmapRatio);
+		}
+		if(calculateCentre) {
+			calculateCentre = false;
+			vX1 = (getMeasuredWidth() - scaledViolin.getWidth()) / 2;
+			vY1 = (getMeasuredHeight() - scaledViolin.getHeight()) / 2;
+			vX2 = vX1 + scaledViolin.getWidth();
+			vY2 = vY1 + scaledViolin.getHeight();
+		}
+		Log.i("violin", "vX1 = " + vX1 + "  - vY1 = " + vY1);		
+		canvas.drawBitmap(scaledViolin, vX1, vY1, violinColour);
 
+		if (bowDown)
+			canvas.drawBitmap(bow, (float) ((lastX + (xOff * 80)) - (bow
+					.getWidth() / 2)), lastY - (bow.getHeight() / 2),
+					violinColour);
+		if (resize) {
+
+			drawDottedLine(canvas, vX1, vY1, vX2, vY1, subtleColour);
+			drawDottedLine(canvas, vX1, vY1, vX1, vY2, subtleColour);
+
+			drawDottedLine(canvas, vX1, vY2, vX2, vY2, subtleColour);
+			drawDottedLine(canvas, vX2, vY1, vX2, vY2, subtleColour);
+
+			
+			
+		} else {
+			canvas.drawLine(vX1, vY1, vX1+touchArea, vY1, subtleColour);
+			canvas.drawLine(vX1, vY1, vX1, vY1+touchArea, subtleColour);
+			
+			canvas.drawLine(vX1, vY2, vX1+touchArea, vY2, subtleColour);
+			canvas.drawLine(vX1, vY2, vX1, vY2-touchArea, subtleColour);
+
+			canvas.drawLine(vX2, vY1, vX2-touchArea, vY1, subtleColour);
+			canvas.drawLine(vX2, vY1, vX2, vY1+touchArea, subtleColour);
+
+			canvas.drawLine(vX2, vY2, vX2-touchArea, vY2, subtleColour);
+			canvas.drawLine(vX2, vY2, vX2, vY2-touchArea, subtleColour);
+
+		}
+
+	}
+	
+	private void drawDottedLine(Canvas canvas, float startX, float startY, float stopX, float stopY, Paint paint) {
+		int stop = 10;
+		if(startX-stopX == 0) {
+			for(float y = startY; y < stopY; y+=stop) {
+				canvas.drawLine(startX, y, stopX, (float)(y+(.5*stop)), subtleColour);
+			}			
+		} else {
+			for(float x = startX; x < stopX; x+=stop) {
+				canvas.drawLine(x, startY, (float)(x+(.5*stop)), stopY, subtleColour);
+			}				
+		}
+		
+
+		
 	}
 
 }
