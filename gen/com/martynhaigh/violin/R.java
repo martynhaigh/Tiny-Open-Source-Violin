abb9ed7cbc5b4caa1ed93b086fd25520776e6f5b
Martyn Haigh <martyn.haigh@gmail.com>
2011-09-16 15:46:07 +0100

    Update to version 1.3

--- gen/com/martynhaigh/violin/R.java
+++ gen/com/martynhaigh/violin/R.java
@@ -23,12 +23,20 @@ public final class R {
         public static final int bow=0x7f020000;
         public static final int icon=0x7f020001;
         public static final int violin=0x7f020002;
+        public static final int widget_frame=0x7f020003;
     }
     public static final class id {
+        public static final int artistname=0x7f080001;
+        public static final int musicspinner=0x7f080004;
+        public static final int selection=0x7f080003;
+        public static final int trackname=0x7f080002;
         public static final int violin=0x7f080000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int music_select_spinner_item=0x7f030001;
+        public static final int musicselect=0x7f030002;
+        public static final int widget=0x7f030003;
     }
     public static final class raw {
         public static final int violinmusic1=0x7f040000;
@@ -38,6 +46,7 @@ public final class R {
     public static final class string {
         public static final int about_text=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int music_select=0x7f050004;
         public static final int ok=0x7f050003;
         public static final int whatsNewText=0x7f050002;
     }
